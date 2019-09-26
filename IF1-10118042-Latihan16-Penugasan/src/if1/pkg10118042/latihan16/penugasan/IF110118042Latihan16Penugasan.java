f0df44d53a837319f763801e7b9671411b331f11
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:19:51 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan16-Penugasan/src/if1/pkg10118042/latihan16/penugasan/IF110118042Latihan16Penugasan.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan16.penugasan;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk Menunjukkan Penugasan
+ */
+public class IF110118042Latihan16Penugasan {
+
+    
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       int c = 0;
+       
+       System.out.println("Nilai a = " + a);
+       System.out.println("Nilai b = " + b);
+       System.out.println("Nilai c = " + c+ "\n");
+       
+       c = a + b;
+       System.out.println("c = a + b = " +c);
+       c += a;
+       System.out.println("c += a = " +c);
+       c -= a;
+       System.out.println("c -= a = " +c);
+       c *= a;
+       System.out.println("c *= a = " +c);
+       
+       a = 10;
+       c = 15;
+       c /= a;
+       System.out.println("c /= a = " +c);
+       
+       a = 10;
+       c = 15;
+       c %= a;
+       System.out.println("c %= a = " +c);
+       c <<= 2;
+       System.out.println("c <<= 2 = " +c);
+       c >>= 2;
+       System.out.println("c >>= 2 = " +c);
+       c >>= 2;
+       System.out.println("c >>= a = " +c);
+       c &= a;
+       System.out.println("c &= a = " +c);
+       c ^= a;
+       System.out.println("c ^= a = " +c);
+       c |= a;
+       System.out.println("c |= a = " +c);
+       
+       
+       
+    
+    }
+    
+}
