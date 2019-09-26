60f3c9a3484584365b566bc8d6058703417922b1
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:16:30 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan12-Aritmatika/src/if1/pkg10118042/latihan12/aritmatika/IF110118042Latihan12Aritmatika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan12.aritmatika;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan Aritmatika
+ */
+public class IF110118042Latihan12Aritmatika {
+
+   
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
