169efc271836155fc263dbb260b242b243996aaf
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:12:43 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan10-Logika/src/if1/pkg10118042/latihan10/logika/IF110118042Latihan10Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan10.logika;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan Logika pada Java
+ */
+public class IF110118042Latihan10Logika {
+
+    
+    public static void main(String[] args) {
+        int umur =19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
