fd3beaf895fd26b8e7c7b071557b4a5468e0563a
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:17:29 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan13-Relational/src/if1/pkg10118042/latihan13/relational/IF110118042Latihan13Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan13.relational;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan Relational
+ */
+public class IF110118042Latihan13Relational {
+
+    
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " +(bil1 == bil2));
+        System.out.println("a != b = " +(bil1 != bil2));
+        System.out.println("a > b = " +(bil1 > bil2));
+        System.out.println("a < b = " +(bil1 < bil2));
+        System.out.println("a >= b = " +(bil2 >= bil1));
+        System.out.println("a <= b = " +(bil2 <= bil1));
+    }
+    
+}
