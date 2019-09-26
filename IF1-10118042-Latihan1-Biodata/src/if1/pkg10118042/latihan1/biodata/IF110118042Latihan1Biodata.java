7222d64b737c78b63884bf08bd0230884aac5787
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:05:38 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan1-Biodata/src/if1/pkg10118042/latihan1/biodata/IF110118042Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan1.biodata;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan biodata
+ * 
+ */
+public class IF110118042Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("BIODATA ROCKSTAR" + "\n" +"------------------");
+        System.out.println("NAMA\t\t: Dhimas Aji Ghairrahmat");
+        System.out.println("ALAMAT\t\t: Jalan Cibatu 12 no.11a");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 1 September 2000");
+        System.out.println("ALAMAT\t\t: Jalan Cibatu 12 no.11a");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t: Baca Komik");
+        System.out.println("MAKES\t\t: Keju");
+        System.out.println("MIKES\t\t: Teh");
+    }
+    
+}
