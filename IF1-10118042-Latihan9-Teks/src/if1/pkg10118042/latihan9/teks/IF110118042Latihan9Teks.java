37e94d2d634d5875e15f8af23edec848b3b2fd60
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:11:48 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan9-Teks/src/if1/pkg10118042/latihan9/teks/IF110118042Latihan9Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan9.teks;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan variabel Teks
+ * 
+ */
+public class IF110118042Latihan9Teks {
+
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: "+tab+ "berisi "+ c);
+    }
+    
+}
