dc55c731d132652025d7c8414d72ba17efd35698
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:18:46 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan15-Logika/src/if1/pkg10118042/latihan15/logika/IF110118042Latihan15Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan15.logika;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk Menunjukkan Logika pada boolean
+ */
+public class IF110118042Latihan15Logika {
+
+    
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("!a = " + !a);
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
