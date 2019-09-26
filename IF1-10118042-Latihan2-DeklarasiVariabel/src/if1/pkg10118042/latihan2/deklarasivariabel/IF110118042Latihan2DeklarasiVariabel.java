90a29ee7649ee1b044849312dc70bf92a506730c
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:06:48 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan2-DeklarasiVariabel/src/if1/pkg10118042/latihan2/deklarasivariabel/IF110118042Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan2.deklarasivariabel;
+
+import java.time.Clock;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk mendeklarasi kan variabel
+ */
+public class IF110118042Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta Uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi Nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi Variabel nilai = " + nilaiInt );
+        System.out.println("Isi Variabel PHI = " + PHI );
+        System.out.println("Isi Variabel nilai = " + nilaiLogika );
+        System.out.println("Isi Variabel nilai = " + nilaiKarakter );
+    }
+    
+}
