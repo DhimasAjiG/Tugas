d8c987fe6e7a8fb2b53d89b111221a6bd74d33c9
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-09-26 16:11:44 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan8-Pecahan/src/if1/pkg10118042/latihan8/pecahan/IF110118042Latihan8Pecahan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan8.pecahan;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menampilkan Variabel Pecahan
+ */
+public class IF110118042Latihan8Pecahan {
+
+    
+    public static void main(String[] args) {
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " +f);
+        System.out.println("Double : " +d);
+    }
+    
+}
